dc6a7ca0ecd5e10466c33733ab2baddfb8d0c540
oyo <oyo@Administrators-MacBook-Pro-154.local>
2020-11-01 00:18:39 +0530

    UpdateView & DeleteView

--- music/urls.py
+++ music/urls.py
@@ -11,4 +11,11 @@ urlpatterns = [
 
     #/music/album/add
     url(r'^album/add/$', views.AlbumCreate.as_view(), name='add-album'),
+
+    #/music/album/2/
+    url(r'^album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+
+    #/music/album/2/delete
+    url(r'^album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete')
+
 ]
--- music/views.py
+++ music/views.py
@@ -1,13 +1,14 @@
 from django.views import generic
 from .models import Album
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 
 class IndexView(generic.ListView):
     template_name = "music/index.html"
 
     #Default variable name that is sent to template will be object_list
     #to change the variable name set 'context_object_name' to desired variable name
-    context_object_name = 'all_albums'
+    context_object_name = 'albums'
 
     def get_queryset(self):
         return Album.objects.all()
@@ -24,3 +25,12 @@ class AlbumCreate(CreateView):
     model = Album
     fields = ['artist', 'album_title', 'genre', 'album_logo']
 
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+
+class AlbumDelete(DeleteView):
+    model = Album
+    success_url = reverse_lazy('music:index')
